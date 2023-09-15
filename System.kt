560f2753759b2653b43b45dc1e2552ca2e0eefe2
BEOBMA <83067179+BEOBMA@users.noreply.github.com>
2023-09-15 11:40:13 +0900

    Update System.kt
    누석

--- System.kt
+++ System.kt
@@ -10,6 +10,7 @@ import org.bukkit.event.player.PlayerJoinEvent
 
 class System : JavaPlugin(), Listener {
 
+//서버가 활성화될 때 이벤트를 등록하고 콘솔에 메시지를 보냅니다.
     override fun onEnable() {
         server.pluginManager.registerEvents(this, this)
         server.pluginManager.registerEvents(Town(), this)
@@ -21,11 +22,12 @@ class System : JavaPlugin(), Listener {
         Bukkit.getConsoleSender().sendMessage("시스템이 활성화되었습니다.")
 
     }
-
+//서버가 비활성화될 때 메시지를 콘솔창에 보냅니다.
     override fun onDisable() {
         Bukkit.getConsoleSender().sendMessage("시스템이 비활성화되었습니다.")
     }
 
+// 플레이어가 입장할 때 실행됩니다. 대체로 공지, 이벤트 등의 구문을 추가하여 사용합니다.
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
         val player = event.player
